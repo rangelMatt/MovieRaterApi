808fa4c894d434bebb82adf1bd7d94252c7540c9
Matt Rangel <mattrangel@gmail.com>
2022-08-25 11:31:58 -0700

    added CRUD for movie ratings

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -15,14 +16,27 @@ class MovieViewSet(viewsets.ModelViewSet):
         if 'stars' in request.data:
 
             movie = Movie.objects.get(id=pk)
-            print('movie title', movie.title)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
 
-            response = {'message': 'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+
         else:
             response = {'message': 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class RatingViewSet(viewsets.ModelViewSet):
