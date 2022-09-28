8b87b455781ab34a215aa6a5aa59e5f63ec72bd9
Matt Rangel <mattrangel@gmail.com>
2022-09-28 15:10:34 -0700

    reestablishing creating user

--- api/models.py
+++ api/models.py
@@ -1,25 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth import get_user_model
 
 
 class Movie(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
-
-    # creator = models.ForeignKey(
-    #     get_user_model(),
-    #     null=True,
-    #     on_delete=models.CASCADE
-    # )
-
-    def __str__(self):
-        return self.title
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def no_of_ratings(self):
         ratings = Rating.objects.filter(movie=self)
@@ -30,6 +17,7 @@ class Movie(models.Model):
         ratings = Rating.objects.filter(movie=self)
         for rating in ratings:
             sum += rating.stars
+
         if len(ratings) > 0:
             return sum / len(ratings)
         else:
@@ -44,6 +32,3 @@ class Rating(models.Model):
     class Meta:
         unique_together = (('user', 'movie'),)
         index_together = (('user', 'movie'),)
-
-    def __str__(self):
-        return self.user
